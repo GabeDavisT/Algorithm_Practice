cfdaac5a0e7675a057a2954d467c7d1fc4d68075
GabeDavisT <114760464+GabeDavisT@users.noreply.github.com>
2024-09-02 17:09:58 -0500

    Create DuplicateInteger.py

---
+++ DuplicateInteger.py
@@ -0,0 +1,25 @@
+...
+Duplicate Integer
+Solved 
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Example 1:
+
+Input: nums = [1, 2, 3, 3]
+
+Output: true
+Example 2:
+
+Input: nums = [1, 2, 3, 4]
+
+Output: false
+...
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
